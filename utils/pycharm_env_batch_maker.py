bca8d752cd0791726e527f7524c037c74f8628ba
Ben Avrahami <sample@notreal.fake>
2019-02-26 19:24:48 +0200

    .

--- utils/pycharm_env_batch_maker.py
+++ utils/pycharm_env_batch_maker.py
@@ -1,34 +1,39 @@
 import os
+from string import Template
+from pathlib import Path
+
 
 def pycharm_env_batch_maker(filename):
-  years = range(2020, 2015, -1)
-  quart = [3, 2, 1]
-  builds = range(9, -1, -1)
-  versions = ['{}.{}.{}'.format(year, q, b) for year in years for q in quart for b in builds]
-  # print(versions)
+    years = range(2020, 2015, -1)
+    quart = [3, 2, 1]
+    builds = range(9, -1, -1)
+    versions = ('{}.{}.{}'.format(year, q, b) for year in years for q in quart for b in builds)
+    # print(versions)
 
-  prefix = 'if not exist %PYCHARM% SET PYCHARM='
-  pf64 = r'C:\Program Files'
-  pf32 = r'C:\Program Files (x86)'
-  jb = r'JetBrains\PyCharm '
-  community = r'Community Edition '
-  bin64 = 'bin\pycharm64.exe'
-  bin32 = 'bin\pycharm.exe'
-  with open(filename, mode='w') as f:
-    f.write('@echo off\n')
-    f.write('set PYCHARM="xxxxxx"\n\n')
-    for v in versions:
-      if v.endswith('.0'):
-        v = v[:-2]
-      for path in [os.path.join(pf64, jb + v, bin64),
-                   os.path.join(pf64, jb + community + v, bin64),
-                   os.path.join(pf32, jb + v, bin32),
-                   os.path.join(pf32, jb + community + v, bin32)]:
-        f.write('{}"{}"\n'.format(prefix, path))
-      f.write('\n')
-    f.write('echo %PYCHARM%\n')
-    f.write('if not exist %PYCHARM% exit /b 3')
+    prefix = 'if not exist %PYCHARM% SET PYCHARM='
+    pf64 = Path(r'C:\Program Files')
+    pf32 = Path(r'C:\Program Files (x86)')
+    jb = r'JetBrains\PyCharm '
+    community = r'Community Edition '
+    bin64 = r'bin\pycharm64.exe'
+    bin32 = r'bin\pycharm.exe'
+
+    with open(filename, mode='w') as f:
+        f.write('@echo off\n')
+        f.write('set PYCHARM="xxxxxx"\n\n')
+        for v in versions:
+            if v.endswith('.0'):
+                v = v[:-2]
+            f.write(':: version '+v)
+            for pf in (pf64, pf32):
+                for comm in (community, ''):
+                    for bin_ in (bin64, bin32):
+                        path = pf / (jb + comm + v) / bin_
+                        f.write('{}"{}"\n'.format(prefix, path))
+            f.write('\n')
+        f.write('echo %PYCHARM%\n')
+        f.write('if not exist %PYCHARM% exit /b 3')
 
 
 if __name__ == '__main__':
-  pycharm_env_batch_maker(r'..\bat\pycharm_env.bat')
+    pycharm_env_batch_maker(r'..\bat\pycharm_env.bat')
